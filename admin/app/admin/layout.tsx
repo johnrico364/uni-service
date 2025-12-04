e5b2b2f4f7d753523f726ae554a26e5853d39d57
John Anthony Rico <johnrico364@gmail.com>
2025-12-04 22:29:10 +0800

    Add all the navigation routes on the navbar

--- admin/app/admin/layout.tsx
+++ admin/app/admin/layout.tsx
@@ -1,21 +1,96 @@
-import type { Metadata } from "next";
-import { LuChartColumnBig } from "react-icons/lu";
+"use client";
+
+import { useRouter } from "next/navigation";
 
 // Icons
 import { MdOutlineArrowForwardIos } from "react-icons/md";
+import { LiaUserSolid } from "react-icons/lia";
+import {
+  LuCalendarCheck2,
+  LuChartColumnBig,
+  LuHandHelping,
+} from "react-icons/lu";
+import { FaRegBell, FaStore } from "react-icons/fa";
+import { BsCartCheck } from "react-icons/bs";
+import { TbDrone, TbReportAnalytics } from "react-icons/tb";
+import { GiReceiveMoney } from "react-icons/gi";
+import { GrDocumentUser } from "react-icons/gr";
+import { PiGear } from "react-icons/pi";
 
-export const metadata: Metadata = {
-  title: "Uni Service Dashboard",
-};
+const routes = [
+  {
+    name: "Dashboard",
+    href: "/admin/dashboard",
+    icon: <LuChartColumnBig className="text-[1.35rem]" />,
+  },
+  {
+    name: "Users",
+    href: "/admin/users",
+    icon: <LiaUserSolid className="text-[1.35rem]" />,
+  },
+  {
+    name: "Providers",
+    href: "/admin/providers",
+    icon: <FaStore className="text-[1.35rem]" />,
+  },
+  {
+    name: "Services",
+    href: "/admin/services",
+    icon: <LuHandHelping className="text-[1.35rem]" />,
+  },
+  {
+    name: "Appointments",
+    href: "/admin/appointments",
+    icon: <LuCalendarCheck2 className="text-[1.35rem]" />,
+  },
+  {
+    name: "Orders",
+    href: "/admin/orders",
+    icon: <BsCartCheck className="text-[1.35rem]" />,
+  },
+  {
+    name: "Drones",
+    href: "/admin/drones",
+    icon: <TbDrone className="text-[1.35rem]" />,
+  },
+  {
+    name: "Payments",
+    href: "/admin/payments",
+    icon: <GiReceiveMoney className="text-[1.35rem]" />,
+  },
+  {
+    name: "Reports",
+    href: "/admin/reports",
+    icon: <TbReportAnalytics className="text-[1.35rem]" />,
+  },
+  {
+    name: "Logs",
+    href: "/admin/logs",
+    icon: <GrDocumentUser className="text-[1.35rem]" />,
+  },
+  {
+    name: "Notifications",
+    href: "/admin/notifications",
+    icon: <FaRegBell className="text-[1.35rem]" />,
+  },
+  {
+    name: "Settings",
+    href: "/admin/settings",
+    icon: <PiGear className="text-[1.35rem]" />,
+  },
+];
 
 export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
+  const router = useRouter();
+
   return (
     <div className="drawer lg:drawer-open">
       <input id="my-drawer-4" type="checkbox" className="drawer-toggle" />
       <div className="drawer-content">
         {/* Navbar */}
+
         <nav className="navbar w-full bg-base-300">
           <label
             htmlFor="my-drawer-4"
@@ -42,19 +117,21 @@ export default function RootLayout({
           {/* Sidebar content here */}
           <ul className="menu w-full grow">
             {/* List item */}
-            <li className="mb-2">
-              <button
-                className="is-drawer-close:tooltip is-drawer-close:tooltip-right"
-                data-tip="Dashboard"
-              >
-                {/* Dashboard Icon */}
-                <LuChartColumnBig className="text-[1.35rem]" />
-                <span className="is-drawer-close:hidden">Dashboard</span>
-              </button>
-            </li>
-
-
-            
+            {routes.map((route) => {
+              return (
+                <li className="mb-2" key={route.name}>
+                  <button
+                    className="is-drawer-close:tooltip is-drawer-close:tooltip-right"
+                    data-tip={route.name}
+                    onClick={() => router.push(route.href)}
+                  >
+                    {/* Dashboard Icon */}
+                    {route.icon}
+                    <span className="is-drawer-close:hidden">{route.name}</span>
+                  </button>
+                </li>
+              );
+            })}
           </ul>
         </div>
       </div>
