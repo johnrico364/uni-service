69268fd2a9bb1b3b3b31e5ca67a46fc70a64027d
John Anthony Rico <johnrico364@gmail.com>
2025-12-05 17:20:53 +0800

    add qr code column in order schema

--- server/modules/orders/order.model.js
+++ server/modules/orders/order.model.js
@@ -5,6 +5,7 @@ const orderSchema = new mongoose.Schema(
     buyer_id: { type: String, required: true, ref: "User" },
     seller_id: { type: String, required: true, ref: "User" },
     total_amount: { type: Number, required: true },
+    qr_code: { type: String, required: true },
     delivery_address: { type: String, required: true },
     delivery_lat: { type: Number },
     delivery_lng: { type: Number },
