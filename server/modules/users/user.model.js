911b3403b461c9573a6a8c967e55190146f5f9a7
John Anthony Rico <johnrico364@gmail.com>
2025-12-18 00:10:58 +0800

    Add soft delete fields

--- server/modules/users/user.model.js
+++ server/modules/users/user.model.js
@@ -25,6 +25,11 @@ const userSchema = new mongoose.Schema(
     // Ban management fields
     ban_until: { type: Date, default: null },
     ban_reason: { type: String, default: null },
+
+    // Soft delete fields
+    is_deleted: { type: Boolean, default: false, index: true },
+    deleted_at: { type: Date, default: null },
+    deleted_by: { type: String, ref: "User", default: null },
   },
   { timestamps: true }
 );
