0e7572a1b9288a396f371c2274bea24e5a5317e1
Ken <kentflores0000@gmail.com>
2025-12-21 16:52:27 +0800

    fix api key

--- user/lib/main.dart
+++ user/lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
-void main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
@@ -13,11 +13,32 @@ void main() async {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Uni Service',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        useMaterial3: true,
+      ),
+      home: const SplashPage(),
+    );
+  }
+}
+
+class SplashPage extends StatelessWidget {
+  const SplashPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp()
-      
+    return const Scaffold(
+      body: Center(
+        child: Text(
+          '🔥 Firebase Connected',
+          style: TextStyle(fontSize: 20),
+        ),
+      ),
+    );
   }
 }
-  
