2ac1cccd394c2dc6f534b95507f411e8f2ccb739
John Anthony Rico <johnrico364@gmail.com>
2026-01-02 11:06:17 +0800

    Update muler image upload

--- server/middlewear/multer.js
+++ server/middlewear/multer.js
@@ -3,14 +3,35 @@ import path from "path";
 
 const storage = multer.diskStorage({
   destination: (req, file, callback) => {
+    console.log(file.mimetype, file.originalname);
     const img_type = req?.body?.image_type;
     callback(null, path.join("images/", img_type));
   },
   filename: (req, file, callback) => {
-    callback(null, Date.now() + ".png");
+    const ext = path.extname(file.originalname);
+    callback(null, Date.now() + "-" + Math.random() + ext);
   },
 });
 
-const upload = multer({ storage });
+const fileFilter = (req, file, callback) => {
+  const allowedTypes = /jpeg|jpg|png/;
+  const isValid =
+    allowedTypes.test(file.mimetype) &&
+    allowedTypes.test(path.extname(file.originalname).toLocaleLowerCase());
+
+  if (isValid) {
+    callback(null, true);
+  } else {
+    callback(new Error("Only image files are allowed"));
+  }
+};
+
+const upload = multer({
+  storage,
+  limits: {
+    fileSize: 3 * 1024 * 1024, // Limit 3 MB
+  },
+  fileFilter,
+});
 
 export default upload;
