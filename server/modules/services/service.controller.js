9fa65ce9079059c57796ccb8203b06f2200bc9c9
John Anthony Rico <johnrico364@gmail.com>
2026-01-04 00:38:27 +0800

    Add create service api

--- server/modules/services/service.controller.js
+++ server/modules/services/service.controller.js
@@ -1,3 +1,10 @@
+import { serviceService } from "./service.service.js";
+
 export const registerService = async (req, res) => {
-  
-}
+  try {
+    const service = await serviceService.registerService(req);
+    res.status(201).json({ success: true, data: service });
+  } catch (error) {
+    res.status(400).json({ success: false, message: error.message });
+  }
+};
